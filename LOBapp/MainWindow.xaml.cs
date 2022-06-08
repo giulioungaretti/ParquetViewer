f7cca9a7cbf36bf06dc1d990055bdaf627dfc96d
Giulio Ungaretti <giungare@microsoft.com>
2022-06-08 21:31:48 +0200

    Add file open action and info bar

--- LOBapp/MainWindow.xaml.cs
+++ LOBapp/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.UI.Xaml;
 
+using System;
+using WinRT.Interop;
+
+using Windows.Storage.Pickers;
 
 namespace LOBapp
 {
@@ -11,18 +15,37 @@ namespace LOBapp
     public sealed partial class MainWindow : Window
     {
 
-
         public MainWindow()
         {
             this.InitializeComponent();
-            BackDrop backdrop  = new BackDrop(this);
+            BackDrop backdrop  = new(this);
             backdrop.SetBackdrop();
+            Title = "Open file";
             
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
             myButton.Content = "Clicked";
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var filePicker = new FileOpenPicker();
+            filePicker.FileTypeFilter.Add("*");
+
+            var hwnd = WindowNative.GetWindowHandle(this);
+            InitializeWithWindow.Initialize(filePicker, hwnd);
+
+            var file = await filePicker.PickSingleFileAsync();    
+            if (file != null)
+            {
+                Title = file.Path;
+                InfoBar.Title = "Loaded file";
+                InfoBar.Message = file.Path;
+                InfoBar.IsOpen = true;
+            }
+
         }
     }
 }
