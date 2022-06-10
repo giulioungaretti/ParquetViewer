fd82df12b91b2d735a7454ca1858b14e63c8b00f
giulio ungaretti <giungare@microsoft.com>
2022-06-10 09:34:36 +0200

    update main windo with body frame

--- LOBapp/MainWindow.xaml.cs
+++ LOBapp/MainWindow.xaml.cs
@@ -1,51 +1,27 @@
-﻿using Microsoft.UI.Xaml;
+﻿using LOBapp.Services;
+using LOBapp.ViewModels;
 
-using System;
-using WinRT.Interop;
+using Microsoft.UI.Xaml;
 
-using Windows.Storage.Pickers;
+namespace LOBapp;
 
-namespace LOBapp
+public sealed partial class MainWindow : Window
 {
-
-
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    public MainWindow()
     {
+        this.InitializeComponent();
+        ViewModel = new MainPageViewModel(this);
 
-        public MainWindow()
-        {
-            this.InitializeComponent();
-            BackDrop backdrop  = new(this);
-            backdrop.SetBackdrop();
-            Title = "Open file";
-            
-        }
+        BackDrop backdrop = new(this);
+        backdrop.SetBackdrop();
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
-
-        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
-        {
-            var filePicker = new FileOpenPicker();
-            filePicker.FileTypeFilter.Add("*");
-
-            var hwnd = WindowNative.GetWindowHandle(this);
-            InitializeWithWindow.Initialize(filePicker, hwnd);
-
-            var file = await filePicker.PickSingleFileAsync();    
-            if (file != null)
-            {
-                Title = file.Path;
-                InfoBar.Title = "Loaded file";
-                InfoBar.Message = file.Path;
-                InfoBar.IsOpen = true;
-            }
-
-        }
+        // TODO: style title bar
+        //ExtendsContentIntoTitleBar = true;
+        //SetTitleBar(AppTitleBar);
     }
+
+    public MainPageViewModel ViewModel { get;}
+    public void SetBody(UIElement uiElement) => Body.Content = uiElement;
+
+    public UIElement GetBody() => Body;
 }
