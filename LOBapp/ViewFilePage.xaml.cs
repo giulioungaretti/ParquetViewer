45fbbec61818dff494b01620b61acf2f9d2139a5
giulio ungaretti <giungare@microsoft.com>
2022-06-10 09:34:58 +0200

    View single file page

---
+++ LOBapp/ViewFilePage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.WinUI.UI.Controls;
+
+using LOBapp.ViewModels;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+
+using Parquet.Data;
+
+namespace LOBapp;
+
+public sealed partial class ViewFilePage : Page
+{
+    public ViewFilePageViewModel ViewModel { get; }
+
+    public ViewFilePage(ViewFilePageViewModel vm)
+    {
+        this.InitializeComponent();
+        this.ViewModel = vm;
+
+        AddColumnsHeader();
+    }
+
+    // Add columns from schema
+    // TODO: we have the parquet types could be fun to use them for the coulmns
+    private void AddColumnsHeader()
+    {
+        int i = 0;
+        foreach (Field f in ViewModel.Table.Schema.Fields)
+        {
+            ParquetDataGrid.Columns.Add(new DataGridTextColumn
+            {
+                Header = f.Name,
+                Width = DataGridLength.SizeToCells,
+                Binding = new Binding
+                {
+                    Path = new PropertyPath("[" + i++ + "]")
+                }
+            });
+
+        }
+    }
+
+    private void ParquetDataGrid_Loaded(object sender, RoutedEventArgs e)
+    {
+
+
+    }
+}
+
---
+++ LOBapp/ViewModels/ViewFilePageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using LOBapp.Services;
+
+using Microsoft.UI.Xaml.Controls;
+
+using Parquet.Data.Rows;
+
+using System;
+using System.Collections;
+
+using Windows.Storage.Pickers;
+
+using WinRT.Interop;
+
+namespace LOBapp.ViewModels;
+
+public class RowView
+{
+    private readonly Row _parquetRow;
+
+    public RowView(Row row)
+    {
+        this._parquetRow = row;
+    }
+
+    public object this[int i]
+    {
+        get
+        {
+            return _parquetRow[i];
+        }
+    }
+}
+public partial class ViewFilePageViewModel : ObservableObject
+{
+
+    public ViewFilePageViewModel(Table table, string path)
+    {
+        Table = table;
+        Path = path;
+    }
+    public Table Table { get; }
+    public string Path { get; }
+}
